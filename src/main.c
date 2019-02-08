90d2e93a0e4bdfaed0cf64549e966407a22f2f50
Atom735 <Atom735.ag47@gmail.com>
2019-02-08 21:13:04 +0300

    Ferst steps

--- src/main.c
+++ src/main.c
@@ -1,6 +1,31 @@
+#include <windows.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 int main ( int argc, char const *argv[] )
 {
+    FILE * CONST pF  = fopen ( "src/main.c", "rb" );
+    fseek ( pF, 0, SEEK_END );
+    CONST UINT nSize = ftell ( pF );
+    fseek ( pF, 0, SEEK_SET );
+    BYTE * CONST pFileBuf = ( BYTE* ) malloc ( nSize );
+
+    UINT nSizeReaded = 0;
+    while ( nSizeReaded < nSize ) {
+        nSizeReaded += fread ( pFileBuf + nSizeReaded, 1, nSize - nSizeReaded, pF );
+    }
+
+    fclose ( pF );
+
+
+    FILE * CONST pFOut  = fopen ( "src/main.x", "wb" );
+
+    UINT nSizeWrited = 0;
+    while ( nSizeWrited < nSize ) {
+        nSizeWrited += fwrite ( pFileBuf + nSizeWrited, 1, nSize - nSizeWrited, pFOut );
+    }
+
+    fclose ( pFOut );
+
     return 0;
 }
